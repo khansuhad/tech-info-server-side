e0c5a9664449d151d3e6519e6e603d10c5271ab9
khan suhad <suhadahmodkhan@gmail.com>
2023-10-19 02:04:59 +0600

    delete method in cart is successfull

--- Index.js
+++ Index.js
@@ -81,6 +81,14 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/carts/:id' , async(req , res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await cartCollection.deleteOne(query);
+        res.send(result)
+
+    })
+
     app.post('/carts' ,async(req , res) => {
         const cart = req.body ;
         const result = await cartCollection.insertOne(cart);
